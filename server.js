261eec70b442b191af16ac6e5c87aef581afa70c
Ibrahima Niass <cheroubawan@hotmail.fr>
2019-10-23 10:35:27 +0200

    Added bcrypt to hash password before storing it in database, security is important

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 const express = require("express");
-const bodyParser = require('body-parser')
+const bodyParser = require("body-parser");
+const bcrypt = require("bcrypt");
 
 const app = express();
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 
 const database = {
   users: [
@@ -28,43 +29,52 @@ app.get("/", (req, res) => {
 });
 
 app.post("/signin", (req, res) => {
-  if (
-    req.body.email === database.users[0].email &&
-    req.body.password === database.users[0].password
-  ) {
-    res.json("you successfully signin");
-  } else {
-    res.status(400).json('error loggin in, please try again')
-  }
+  const password = req.body.password;
+  const hash = database.users[database.users.length - 1].password;
+  bcrypt.compare(password, hash, function(err, resp) {
+    // res == true
+    if (resp) {
+      console.log(resp)
+      res.json("you successfully signin");
+    } else {
+      console.log(resp,password)
+      res.status(400).json("error loggin in, please try again");
+    }
+  });
 });
 
-app.post('/signup', (req,res)=> {
+app.post("/signup", (req, res) => {
   const { email, name, password } = req.body;
   const oldUsers = database.users;
-  const newUser =  {
-    id: 125,
-    name: name,
-    email: email,
-    password: password,
-    joined: new Date()
-  }
-  database.users = [...oldUsers, newUser]
-  res.json(database.users[database.users.length-1])
-})
+  const saltRounds = 10;
+  bcrypt.hash(password, saltRounds, (err, hash) => {
+    // Store hash in your password DB.
+    console.log(hash);
+    const newUser = {
+      id: 125,
+      name: name,
+      email: email,
+      password: hash,
+      joined: new Date()
+    };
+    database.users = [...oldUsers, newUser];
+    res.json(database.users[database.users.length - 1]);
+  });
+});
 
-app.get('/profile/:id', (req, res) => {
+app.get("/profile/:id", (req, res) => {
   const { id } = req.params;
   let found = false;
   database.users.forEach(user => {
-    if(user.id == id ) {
+    if (user.id == id) {
       found = true;
-      return res.json(user)
-    } 
-  })
-  if(!found) {
-    res.status(404).json('No such a user')
+      return res.json(user);
+    }
+  });
+  if (!found) {
+    res.status(404).json("No such a user");
   }
-})
+});
 
 app.listen(3000, () => {
   console.log("app is running on port 3000");
